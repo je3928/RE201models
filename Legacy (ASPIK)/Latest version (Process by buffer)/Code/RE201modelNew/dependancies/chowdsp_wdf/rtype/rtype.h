624d6c9b5441bbbdfc44997d24054510f5e687f0
je3928 <je596688@gmail.com>
2025-04-19 00:08:21 +0100

    Remove legacy files

--- Legacy (ASPIK)/Latest version (Process by buffer)/Code/RE201modelNew/dependancies/chowdsp_wdf/rtype/rtype.h
+++
@@ -1,8 +0,0 @@
-#ifndef CHOWDSP_WDF_RTYPE_H_INCLUDED
-#define CHOWDSP_WDF_RTYPE_H_INCLUDED
-
-#include "rtype_adaptor.h"
-#include "root_rtype_adaptor.h"
-#include "wdf_rtype.h"
-
-#endif // CHOWDSP_WDF_RTYPE_H_INCLUDED
--- Legacy (ASPIK)/Original (Process by sample)/Plugin/ASPIK Plugin (build for 1.9.2)/JESEfinal/win_build/readme.txt
+++
@@ -1,4 +0,0 @@
-Follow the CMake instructions to build your project; see the ASPiK Documentation for more information if you do not know how. 
-
-http://aspikplugins.com/sdkdocs/html/cmake_running.html
-
